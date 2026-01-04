a154038399c29d4095bd77abc09b1503e15668d9
scott.mcginn1978 <scott.mcginn1978@gmail.com>
2026-01-04 21:05:43 +0000

    Update react-native.config.js with explicit source directories for android modules

--- react-native.config.js
+++ react-native.config.js
@@ -6,25 +6,33 @@ module.exports = {
   dependencies: {
     '@react-native-async-storage/async-storage': {
       platforms: {
-        android: {},
+        android: {
+          sourceDir: '../node_modules/@react-native-async-storage/async-storage/android',
+        },
         ios: {},
       },
     },
     'react-native-gesture-handler': {
       platforms: {
-        android: {},
+        android: {
+          sourceDir: '../node_modules/react-native-gesture-handler/android',
+        },
         ios: {},
       },
     },
     'react-native-safe-area-context': {
       platforms: {
-        android: {},
+        android: {
+          sourceDir: '../node_modules/react-native-safe-area-context/android',
+        },
         ios: {},
       },
     },
     'react-native-screens': {
       platforms: {
-        android: {},
+        android: {
+          sourceDir: '../node_modules/react-native-screens/android',
+        },
         ios: {},
       },
     },
