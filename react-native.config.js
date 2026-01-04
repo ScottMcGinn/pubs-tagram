b1f5f8f3021b0708f9f7fa6652650ce34f75d6fe
scott.mcginn1978 <scott.mcginn1978@gmail.com>
2026-01-04 21:19:49 +0000

    Fix: Revert native modules from plugins array, use react-native.config.js for standard autolinking instead

--- react-native.config.js
+++ react-native.config.js
@@ -6,33 +6,25 @@ module.exports = {
   dependencies: {
     '@react-native-async-storage/async-storage': {
       platforms: {
-        android: {
-          sourceDir: './node_modules/@react-native-async-storage/async-storage/android',
-        },
+        android: {},
         ios: {},
       },
     },
     'react-native-gesture-handler': {
       platforms: {
-        android: {
-          sourceDir: './node_modules/react-native-gesture-handler/android',
-        },
+        android: {},
         ios: {},
       },
     },
     'react-native-safe-area-context': {
       platforms: {
-        android: {
-          sourceDir: './node_modules/react-native-safe-area-context/android',
-        },
+        android: {},
         ios: {},
       },
     },
     'react-native-screens': {
       platforms: {
-        android: {
-          sourceDir: './node_modules/react-native-screens/android',
-        },
+        android: {},
         ios: {},
       },
     },
