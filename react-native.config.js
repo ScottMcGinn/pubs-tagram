5d1c84e91592ddf202d0f76c177f37258b776c07
scott.mcginn1978 <scott.mcginn1978@gmail.com>
2026-01-04 21:02:45 +0000

    Add react-native.config.js for proper native module autolinking

---
+++ react-native.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  dependencies: {
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-gesture-handler': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-safe-area-context': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-screens': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+  },
+};
